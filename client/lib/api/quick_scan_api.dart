d69b9d5833fa3d8d5a836bce16b94e34774b200a
Daniel Walbolt <dwalbolt@gmail.com>
2025-03-09 21:58:19 -0400

    Add mock API client route for getting group information

--- client/lib/api/quick_scan_api.dart
+++ client/lib/api/quick_scan_api.dart
@@ -1,12 +1,13 @@
 import 'package:get/get.dart';
 import 'package:quick_attendance/api/_api_client.dart';
 import 'package:quick_attendance/controllers/auth_controller.dart';
+import 'package:quick_attendance/models/group_model.dart';
 
 /// The client for sending requests to the Attenda Scan API
 class QuickScanApi {
   final apiClient = BaseApiClient("http://localhost:8080/quick-scan-api");
 
-  /// Example client
+  /// Example
   Future<Response> getData({required String groupCode}) async {
     return await apiClient.get("/foobar/za", query: {"groupCode": groupCode});
   }
@@ -25,5 +26,21 @@ class QuickScanApi {
       "last_name": lastName,
       "password": password,
     });
+  }
+
+  Future<GroupModel?> getGroup({required String groupId}) async {
+    final Response response = await apiClient.get(
+      "",
+      query: {"groupId": groupId},
+    );
+    if (response.statusCode == 200) {
+      return GroupModel.fromJson(response.body);
+    } else if (response.statusCode == 404) {
+      return null;
+    } else {
+      throw Exception(
+        "Server failed to determine if group: '$groupId' exists.",
+      );
+    }
   }
 }
