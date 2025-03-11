1cdf0b8fe9cc0f08190ab11c4e4c78dacf269cff
Daniel Walbolt <dwalbolt@gmail.com>
2025-03-11 10:04:01 -0400

    Adjust account model to expose reactive variables and read proper values from json

--- client/lib/models/account_model.dart
+++ client/lib/models/account_model.dart
@@ -4,37 +4,42 @@ import 'package:quick_attendance/models/base_api_model.dart';
 final class AccountModel extends BaseApiModel<AccountModel> {
   final String? accountId;
 
-  late final RxString _username;
-  RxString get username => _username;
-
-  late final RxString _firstName;
-  RxString get firstName => _firstName;
-
-  late final RxString _lastName;
-  RxString get lastName => _lastName;
+  late final RxString email;
+  late final RxString username;
+  late final RxString firstName;
+  late final RxString lastName;
 
   AccountModel({
     this.accountId,
+    String email = "",
     String username = "",
     String firstName = "",
     String lastName = "",
   }) {
-    _username = username.obs;
-    _firstName = firstName.obs;
-    _lastName = lastName.obs;
+    this.email = email.obs;
+    this.username = username.obs;
+    this.firstName = firstName.obs;
+    this.lastName = lastName.obs;
   }
 
   factory AccountModel.fromJson(Map<String, dynamic> json) {
     return AccountModel(
       accountId: json["accountId"],
+      email: json["email"],
       username: json["username"],
-      firstName: json["firstName"],
-      lastName: json["lastName"],
+      firstName: json["first_name"],
+      lastName: json["last_name"],
     );
   }
 
   @override
   Map<String, dynamic> toJson() {
-    return {"accountId": accountId, "username": _username.value};
+    return {
+      "accountId": accountId,
+      "email": email.value,
+      "username": username.value,
+      "first_name": firstName.value,
+      "last_name": lastName.value,
+    };
   }
 }
