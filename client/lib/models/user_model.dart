1dff193335c9c9136711fd54c81e42ca96dca30e
Daniel Walbolt <dwalbolt@gmail.com>
2025-03-15 11:09:17 -0400

    Remove id lists from user model (API returns a Set instead of an array)

--- client/lib/models/user_model.dart
+++ client/lib/models/user_model.dart
@@ -7,10 +7,6 @@ final class UserModel extends BaseApiModel<UserModel> {
   late final RxString username;
   late final RxString firstName;
   late final RxString lastName;
-  late final RxList<String> ownedGroupIds;
-  late final RxList<String> managedGroupIds;
-  late final RxList<String> memberGroupIds;
-  late final RxList<String> pendingGroupIds;
 
   UserModel({
     this.userId,
@@ -18,19 +14,11 @@ final class UserModel extends BaseApiModel<UserModel> {
     String? username = "",
     String? firstName = "",
     String? lastName = "",
-    List<String>? ownedGroupIds = const [],
-    List<String>? managedGroupIds = const [],
-    List<String>? memberGroupIds = const [],
-    List<String>? pendingGroupIds = const [],
   }) {
     this.email = (email ?? "").obs;
     this.username = (username ?? "").obs;
     this.firstName = (firstName ?? "").obs;
     this.lastName = (lastName ?? "").obs;
-    this.ownedGroupIds = (ownedGroupIds ?? []).obs;
-    this.managedGroupIds = (managedGroupIds ?? []).obs;
-    this.memberGroupIds = (memberGroupIds ?? []).obs;
-    this.pendingGroupIds = (pendingGroupIds ?? []).obs;
   }
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
@@ -40,10 +28,6 @@ final class UserModel extends BaseApiModel<UserModel> {
       username: json["username"],
       firstName: json["first_name"],
       lastName: json["last_name"],
-      ownedGroupIds: json["fk_owned_group_ids"],
-      managedGroupIds: json["fk_managed_group_ids"],
-      memberGroupIds: json["fk_member_group_ids"],
-      pendingGroupIds: json["fk_pending_group_ids"],
     );
   }
 
@@ -55,10 +39,6 @@ final class UserModel extends BaseApiModel<UserModel> {
       "username": username.value,
       "first_name": firstName.value,
       "last_name": lastName.value,
-      "fk_owned_group_ids": ownedGroupIds.toList(),
-      "fk_managed_group_ids": managedGroupIds.toList(),
-      "fk_member_group_ids": memberGroupIds.toList(),
-      "fk_pending_group_ids": pendingGroupIds.toList(),
     };
   }
 }
