19b02f39d2d24ee9b1541ef5b04e0b64c228940a
Daniel Walbolt <dwalbolt@gmail.com>
2025-03-17 15:58:07 -0400

    Create a skeleton loading widget for a single container

---
+++ client/lib/components/shimmer_skeletons/skeleton_shimmer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+/// A simple element that either displays a loading skeleton animation or
+/// the provided child widget based on the isLoading parameter.
+class SkeletonShimmer extends StatelessWidget {
+  final bool isLoading;
+
+  /// The widget to render when not in loading state.
+  final Widget widget;
+  const SkeletonShimmer({
+    super.key,
+    required this.isLoading,
+    required this.widget,
+  });
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading) {
+      return Shimmer.fromColors(
+        baseColor: Theme.of(context).colorScheme.onSurface.withAlpha(50),
+        highlightColor: Theme.of(context).colorScheme.onSurface.withAlpha(100),
+        period: Duration(seconds: 1),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withValues(alpha: 0.2),
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return widget;
+    }
+  }
+}
