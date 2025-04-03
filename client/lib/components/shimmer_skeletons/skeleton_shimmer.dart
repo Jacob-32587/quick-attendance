175bbde4b7c6e53e3a0fa8eb9fd83f06657f76b7
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-03 09:34:33 -0400

    Utilize binary choice component for loading state widgets

--- client/lib/components/shimmer_skeletons/skeleton_shimmer.dart
+++ client/lib/components/shimmer_skeletons/skeleton_shimmer.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quick_attendance/components/binary_choice.dart';
 import 'package:shimmer/shimmer.dart';
 
 /// A simple element that either displays a loading skeleton animation or
 /// the provided child widget based on the isLoading parameter.
 class SkeletonShimmer extends StatelessWidget {
-  final bool isLoading;
+  final RxBool isLoading;
 
   /// The widget to render when not in loading state.
   final Widget widget;
@@ -15,28 +17,32 @@ class SkeletonShimmer extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return Shimmer.fromColors(
-        baseColor: Theme.of(context).colorScheme.onSurface.withAlpha(50),
-        highlightColor: Theme.of(context).colorScheme.onSurface.withAlpha(100),
-        period: Duration(seconds: 1),
-        child: Container(
-          padding: EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withValues(alpha: 0.2),
-                blurRadius: 8,
-                offset: Offset(0, 4),
-              ),
-            ],
+    return Obx(
+      () => BinaryChoice(
+        choice: isLoading.value,
+        widget1: Shimmer.fromColors(
+          baseColor: Theme.of(context).colorScheme.onSurface.withAlpha(50),
+          highlightColor: Theme.of(
+            context,
+          ).colorScheme.onSurface.withAlpha(100),
+          period: Duration(seconds: 1),
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withValues(alpha: 0.2),
+                  blurRadius: 8,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
           ),
         ),
-      );
-    } else {
-      return widget;
-    }
+        widget2: widget,
+      ),
+    );
   }
 }
