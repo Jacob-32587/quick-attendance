965927e1c4473dafeab231a8d45bc543e252d70f
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-08 10:26:09 -0400

    Create reactive variable for domain and port and add a listener to it to update the apiClient baseUrl

--- client/lib/api/quick_attendance_api.dart
+++ client/lib/api/quick_attendance_api.dart
@@ -6,9 +6,20 @@ import 'package:quick_attendance/models/group_settings_model.dart';
 import 'package:quick_attendance/models/responses/login_response.dart';
 import 'package:quick_attendance/models/user_model.dart';
 
-/// The client for sending requests to the Attenda Scan API
+/// The client for sending requests to the Quick Attendance API
 class QuickAttendanceApi extends GetxService {
+  /// The dynamic domain and port to support demo
+  final RxString domainAndPort = "localhost:8080".obs;
   final apiClient = BaseApiClient("http://localhost:8080/quick-attendance-api");
+
+  @override
+  void onInit() {
+    ever(domainAndPort, (newDomainAndPort) => {
+      apiClient.baseUrl = "http://$newDomainAndPort/quick-attendance-api";
+    });
+
+    super.onInit();
+  }
 
   /// Example
   Future<Response> getData({required String groupCode}) async {
