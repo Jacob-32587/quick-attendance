5ad9d9d975d1c7648a23d73cc025e762d57b3622
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-08 08:45:25 -0400

    Add scroll down to refresh to group pages

--- client/lib/pages/attendance_group/components/group_scroll_view.dart
+++ client/lib/pages/attendance_group/components/group_scroll_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quick_attendance/components/shimmer_skeletons/skeleton_shimmer.dart';
-import 'package:quick_attendance/pages/attendance_group/components/group_page.dart';
+import 'package:quick_attendance/pages/attendance_group/components/url_group_page.dart';
 
 class GroupPageContainer extends StatelessWidget {
   late final GroupController _controller = Get.find();
@@ -10,89 +10,96 @@ class GroupPageContainer extends StatelessWidget {
   final Widget? content;
   final String title;
 
+  Future<void> onRefresh() async {
+    await _controller.fetchGroup(_controller.groupId);
+  }
+
   GroupPageContainer({super.key, required this.title, this.content});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      physics: BouncingScrollPhysics(),
-      slivers: [
-        SliverAppBar(
-          expandedHeight: 300,
-          pinned: true,
-          stretch: true,
-          centerTitle: true,
-          automaticallyImplyLeading: true,
-          elevation: 4, // initial elevation
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Get.toNamed("/");
-            },
-          ),
-          actions: [
-            Obx(
-              () => IconButton(
-                onPressed:
-                    () =>
-                        _controller.isEditingGroup.value =
-                            !_controller.isEditingGroup.value,
-                icon: Icon(
-                  _controller.isEditingGroup.value ? Icons.save : Icons.edit,
-                ),
-              ),
+    return RefreshIndicator(
+      onRefresh: onRefresh,
+      child: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            stretch: true,
+            centerTitle: true,
+            automaticallyImplyLeading: true,
+            elevation: 4, // initial elevation
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Get.toNamed("/");
+              },
             ),
-          ],
-          flexibleSpace: FlexibleSpaceBar(
-            collapseMode: CollapseMode.parallax,
-            background: Image.network(backgroundImageUrl, fit: BoxFit.cover),
-            titlePadding: EdgeInsets.only(bottom: 12, left: 70),
-            expandedTitleScale: 1.5,
-            title: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SkeletonShimmer(
-                  isLoading: _controller.isLoadingGroup,
-                  skeletonHeight: 35,
-                  skeletonWidth: 300,
-                  widget: Text(
-                    title,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black54,
-                          blurRadius: 10,
-                          offset: Offset(1, 1),
-                        ),
-                      ],
-                    ),
+            actions: [
+              Obx(
+                () => IconButton(
+                  onPressed:
+                      () =>
+                          _controller.isEditingGroup.value =
+                              !_controller.isEditingGroup.value,
+                  icon: Icon(
+                    _controller.isEditingGroup.value ? Icons.save : Icons.edit,
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-        SliverToBoxAdapter(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(24),
+              ),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              background: Image.network(backgroundImageUrl, fit: BoxFit.cover),
+              titlePadding: EdgeInsets.only(bottom: 12, left: 70),
+              expandedTitleScale: 1.5,
+              title: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SkeletonShimmer(
+                    isLoading: _controller.isLoadingGroup,
+                    skeletonHeight: 35,
+                    skeletonWidth: 300,
+                    widget: Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black54,
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            padding: const EdgeInsets.only(
-              top: 24,
-              left: 8,
-              right: 8,
-              bottom: 24,
-            ),
-            child: content,
           ),
-        ),
-      ],
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(24),
+                ),
+              ),
+              padding: const EdgeInsets.only(
+                top: 24,
+                left: 8,
+                right: 8,
+                bottom: 24,
+              ),
+              child: content,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
