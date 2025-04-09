5c84cc75860acb4ae904186edaeccb92923dbb00
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-09 10:29:08 -0400

    Create util function for formatting date

--- client/lib/util/time.dart
+++ client/lib/util/time.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 int getWeekOfMonth(DateTime date) {
   final int month = date.month;
   final int year = date.year;
@@ -7,4 +9,31 @@ int getWeekOfMonth(DateTime date) {
 
   final int week = 1 + (offsetDate ~/ 7);
   return week;
+}
+
+String formatDate(DateTime? date) {
+  if (date == null) {
+    return "";
+  }
+
+  final day = date.day;
+  final suffix = _getDaySuffix(day);
+  final month = DateFormat.MMMM().format(date);
+  final year = date.year;
+
+  return '$month $day$suffix, $year';
+}
+
+String _getDaySuffix(int day) {
+  if (day >= 11 && day <= 13) return 'th';
+  switch (day % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
 }
