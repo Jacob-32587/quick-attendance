b5ade9f84a7b4f4f02b9a92f722a184efcff79a4
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-12 16:46:47 -0400

    Add callback function to websocket connect, and create attendanceTaken event listener

--- client/lib/api/quick_attendance_websocket.dart
+++ client/lib/api/quick_attendance_websocket.dart
@@ -6,17 +6,40 @@ import 'package:quick_attendance/controllers/auth_controller.dart';
 class QuickAttendanceWebsocket extends WebSocketService {
   late final QuickAttendanceApi _api = Get.find();
   late final AuthController _auth = Get.find();
-  @override
-  void registerListeners() {}
 
-  void connectToGroupAttendance({required String? groupId}) {
+  /// Define a custom handler for when the attendanceTaken event is received
+  /// from the server.
+  void Function()? attendanceTakenHandler;
+  @override
+  void registerListeners() {
+    var socket = this.socket;
+    if (socket == null) {
+      return;
+    }
+    socket.on("attendanceTaken", (_) {
+      var handler = attendanceTakenHandler;
+      if (handler != null) {
+        handler();
+      }
+    });
+  }
+
+  void connectToGroupAttendance({
+    required String? groupId,
+    void Function()? onConnect,
+    void Function()? onConnectError,
+    void Function()? onDisconnect,
+  }) {
     var domainAndPort = _api.domainAndPort.value;
     super.connectToServer(
       url: "ws://$domainAndPort",
       optionBuilder:
           (defaultOptions) => defaultOptions
               .setQuery({"group_id": groupId})
-              .setAuth({"token": _auth.jwt}),
+              .setAuth({"token": _auth.jwt.value}),
+      onConnect: onConnect,
+      onConnectError: onConnectError,
+      onDisconnect: onDisconnect,
     );
   }
 }
