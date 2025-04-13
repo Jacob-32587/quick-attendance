aad9f056b8c44f74ef36c3aad9971ae1d4eb1c1a
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-13 15:56:44 -0400

    Make a flat_button widget that behaves similarly to primary button

--- client/lib/components/flat_button.dart
+++ client/lib/components/flat_button.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 class FlatButton extends StatelessWidget {
   final void Function()? onPressed;
-  final Widget child;
+  final String? text;
+  final Widget? child;
   final EdgeInsetsGeometry padding;
   final Color? backgroundColor;
   final Color? foregroundColor;
+  final bool isLoading;
 
   const FlatButton({
     super.key,
     required this.onPressed,
-    required this.child,
+    this.child,
     this.padding = const EdgeInsets.symmetric(vertical: 24),
     this.foregroundColor,
     this.backgroundColor,
+    this.isLoading = false,
+    this.text = "",
   });
 
   @override
@@ -30,7 +34,24 @@ class FlatButton extends StatelessWidget {
           foregroundColor:
               foregroundColor ?? Theme.of(context).colorScheme.primary,
         ),
-        child: child,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            (child == null) ? Text(text ?? "Default") : child!,
+            if (isLoading) ...[
+              SizedBox(width: 10),
+              SizedBox(
+                height: 16,
+                width: 16,
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).colorScheme.primary,
+                  strokeWidth: 2,
+                ),
+              ),
+            ],
+          ],
+        ),
       ),
     );
   }
