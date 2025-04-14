f8149881660ae1aab4745a19c792f536c69933ba
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-14 14:26:05 -0400

    Update profile edit screen to have some loading animation(s)

--- client/lib/pages/home/components/profile_header.dart
+++ client/lib/pages/home/components/profile_header.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quick_attendance/components/shimmer_skeletons/skeleton_shimmer.dart';
+import 'package:quick_attendance/controllers/profile_controller.dart';
 
 class ProfileHeader extends StatelessWidget {
+  late final ProfileController _controller = Get.find();
   final String name;
   final String user;
   final String email;
@@ -8,14 +12,15 @@ class ProfileHeader extends StatelessWidget {
     return name.isNotEmpty ? name[0].toUpperCase() : '';
   }
 
-  const ProfileHeader({required this.name, required this.user, required this.email});
+  ProfileHeader({required this.name, required this.user, required this.email});
 
   @override
   Widget build(BuildContext context) {
-    return Container( 
+    return Container(
       width: 500,
       padding: EdgeInsets.all(16.0),
       child: Row(
+        mainAxisSize: MainAxisSize.min,
         children: [
           CircleAvatar(
             radius: 40,
@@ -30,25 +35,36 @@ class ProfileHeader extends StatelessWidget {
                   Shadow(
                     offset: Offset(1.5, 1.5), // Position of the shadow
                     blurRadius: 2.0, // How much the shadow spreads
-                    color: Theme.of(context).colorScheme.primary, // Outline color
+                    color:
+                        Theme.of(context).colorScheme.primary, // Outline color
                   ),
                 ],
               ),
             ),
           ),
           SizedBox(width: 10),
-          Text(
-            name,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          SkeletonShimmer(
+            skeletonWidth: 100,
+            skeletonHeight: 30,
+            isLoading: _controller.isFetchingProfile,
+            widget: Text(
+              name,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
           ),
-          SizedBox(width:10),
-          Text(
-            "($user)",
-            textAlign: TextAlign.left,
-            style: TextStyle(fontSize: 22, color: Colors.grey[500]),
+          SizedBox(width: 10),
+          SkeletonShimmer(
+            skeletonWidth: 100,
+            skeletonHeight: 30,
+            isLoading: _controller.isFetchingProfile,
+            widget: Text(
+              "($user)",
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 22, color: Colors.grey[500]),
+            ),
           ),
         ],
-      )
+      ),
     );
   }
 }
--- client/lib/pages/home/components/profile_info.dart
+++ client/lib/pages/home/components/profile_info.dart
@@ -11,47 +11,47 @@ class ProfileInfo extends StatelessWidget {
   final String lastName;
   final List<GroupModel>? groups;
   final _formKey = GlobalKey<FormState>();
+  final RxBool isSavingProfile = false.obs;
 
-  final ProfileController profileController = Get.find();
+  late final ProfileController profileController = Get.find();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _firstNameController = TextEditingController();
   TextEditingController _lastNameController = TextEditingController();
 
-  ProfileInfo({required this.firstName, required this.lastName, required this.user, required this.email, required this.groups}) {
-    _emailController = TextEditingController(text: '$email');
-    _usernameController = TextEditingController(text: '$user');
-    _firstNameController = TextEditingController(text: '$firstName');
-    _lastNameController = TextEditingController(text: '$lastName');
+  ProfileInfo({
+    super.key,
+    required this.firstName,
+    required this.lastName,
+    required this.user,
+    required this.email,
+    required this.groups,
+  }) {
+    _emailController = TextEditingController(text: email);
+    _usernameController = TextEditingController(text: user);
+    _firstNameController = TextEditingController(text: firstName);
+    _lastNameController = TextEditingController(text: lastName);
   }
 
   void updateInfo() async {
-      if (_formKey.currentState!.validate() == false) {
-        return;
-      }
-      profileController.updateUserAccount(
-        username: _usernameController.text.trim(),
-        email: _emailController.text.trim(),
-        firstName: _firstNameController.text.trim(),
-        lastName: _lastNameController.text.trim()
-      );
+    if (_formKey.currentState!.validate() == false) {
+      return;
+    }
+    isSavingProfile.value = true;
+    await profileController.updateUserAccount(
+      username: _usernameController.text.trim(),
+      email: _emailController.text.trim(),
+      firstName: _firstNameController.text.trim(),
+      lastName: _lastNameController.text.trim(),
+    );
 
-      // Now update the observable user in the controller
-      final updatedUser = profileController.user.value!;
-      updatedUser.username.value = _usernameController.text.trim();
-      updatedUser.email.value = _emailController.text.trim();
-      updatedUser.firstName.value = _firstNameController.text.trim();
-      updatedUser.lastName.value = _lastNameController.text.trim();
-
-      // Trigger UI update
-      profileController.user.refresh();
-      
-      Get.snackbar("Success!", "Profile updated successfully.", snackPosition: SnackPosition.BOTTOM);
+    await profileController.fetchProfileData();
+    isSavingProfile.value = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container( 
+    return Container(
       width: 500,
       padding: EdgeInsets.all(16.0),
       child: Column(
@@ -63,59 +63,64 @@ class ProfileInfo extends StatelessWidget {
           ),
           Divider(
             color: Colors.grey, // Line color
-            thickness: 1,       // Line thickness
-            indent: 5,         // Start padding
-            endIndent: 5,      // End padding
+            thickness: 1, // Line thickness
+            indent: 5, // Start padding
+            endIndent: 5, // End padding
           ),
           Form(
             key: _formKey,
             child: Column(
-     
               children: [
                 SizedBox(height: 10),
-                 TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(labelText: 'Username'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter a valid username";
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(labelText: 'Email'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your email";
-                      }
-                      if (!RegExp(
-                        r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
-                      ).hasMatch(value)) {
-                        return 'Enter a valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _firstNameController,
-                    decoration: InputDecoration(labelText: 'First Name'),
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _lastNameController,
-                    decoration: InputDecoration(labelText: 'Last Name'),
-                  ),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(labelText: 'Username'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter a valid username";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter your email";
+                    }
+                    if (!RegExp(
+                      r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+                    ).hasMatch(value)) {
+                      return 'Enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration(labelText: 'First Name'),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _lastNameController,
+                  decoration: InputDecoration(labelText: 'Last Name'),
+                ),
                 SizedBox(height: 25),
-                PrimaryButton(text: "Save", onPressed: updateInfo),
-                SizedBox(height: 10)
-              ]
-            )
-          )
-        ]
+                Obx(
+                  () => PrimaryButton(
+                    text: "Save",
+                    onPressed: updateInfo,
+                    isLoading: isSavingProfile.value,
+                  ),
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
