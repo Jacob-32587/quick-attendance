67f347307c0792df18003ca52421833bc43f276f
Daniel Walbolt <dwalbolt@gmail.com>
2025-04-14 12:37:07 -0400

    Move history page to home page

--- client/lib/pages/home/history_screen.dart
+++ client/lib/pages/home/history_screen.dart
@@ -1,164 +1,44 @@
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:quick_attendance/api/_api_client.dart';
-import 'package:quick_attendance/api/quick_attendance_api.dart';
-import 'package:quick_attendance/controllers/auth_controller.dart';
 import 'package:quick_attendance/controllers/history_controller.dart';
-import 'package:quick_attendance/models/attendance_history_model.dart';
 import 'package:quick_attendance/util/time.dart';
-
-class _AttendanceEventData {
-  late String attendanceId;
-  late String groupId;
-  _AttendanceEventData({
-    required String attendanceId,
-    required String groupId,
-  }) {
-    this.attendanceId = attendanceId;
-    this.groupId = groupId;
-  }
-
-  String getEventId() {
-    return attendanceId + groupId;
-  }
-}
 
 class HistoryScreen extends StatelessWidget {
   late final HistoryController _historyController = Get.find();
-  late final AuthController _authController = Get.find();
-  final _calendarEventController = EventController<_AttendanceEventData?>();
-  final clearedData = RxBool(false);
-  final currentDate = Rx<DateTime>(DateTime.now());
-
-  late final QuickAttendanceApi _api = Get.find();
-  final attendanceHistory = Rxn<AttendanceHistoryModel>();
 
   HistoryScreen({super.key});
 
-  Future<void> getAttendanceHistoryForWeek(DateTime? time) async {
-    // isLoadingHistory.value = true;
-    final response = await _api.getWeeklyUserAttendance(
-      time?.year,
-      time?.month,
-      getWeekOfMonthNullable(time),
-    );
-    if (response.statusCode == HttpStatusCode.ok) {
-      attendanceHistory.value = response.body;
-    } else {
-      // TODO: What should we do when this request fails
-    }
-    // isLoadingHistory.value = false;
-  }
-
-  Future<void> onRefresh() async {
-    await _historyController.getAttendanceHistoryForWeek(currentDate.value);
-    var events =
-        _historyController.attendanceHistory.value?.attendance
-            .map(
-              (x) => x.attendanceRecords.map(
-                (y) => _getCalendarEventData(
-                  x.groupName.value,
-                  x.groupId.value,
-                  y.attendanceStartTime.value,
-                  y.attendanceEndTime.value,
-                  y.attendanceId.value,
-                  y.present.value,
-                ),
-              ),
-            )
-            .expand((e) => e)
-            .nonNulls
-            .toList() ??
-        [];
-
-    var storedEventsIds =
-        _calendarEventController.allEvents
-            .map((x) => x.event?.getEventId())
-            .nonNulls
-            .toSet();
-
-    var newEvents =
-        events
-            .where((x) => !storedEventsIds.contains(x.event?.getEventId()))
-            .toList();
-
-    if (newEvents.isEmpty) {
-      return;
-    }
-    _calendarEventController.addAll(newEvents);
-  }
-
-  CalendarEventData<_AttendanceEventData>? _getCalendarEventData(
-    String? groupName,
-    String? groupId,
-    DateTime? attendanceStartTime,
-    DateTime? attendanceEndTime,
-    String? attendanceId,
-    bool? present,
-  ) {
-    if (groupName != null &&
-        attendanceStartTime != null &&
-        attendanceEndTime != null &&
-        groupId != null &&
-        attendanceId != null &&
-        present != null) {
-      if (attendanceStartTime.toLocal().day ==
-          attendanceEndTime.toLocal().day) {
-        return CalendarEventData<_AttendanceEventData>(
-          title: groupName,
-          date: attendanceStartTime.toLocal(),
-          endDate: attendanceEndTime.toLocal().add(const Duration(minutes: 10)),
-          startTime: attendanceStartTime.toLocal(),
-          endTime: attendanceEndTime.toLocal().add(const Duration(minutes: 10)),
-          color: present ? Colors.blue : Colors.red,
-          event: _AttendanceEventData(
-            attendanceId: attendanceId,
-            groupId: groupId,
-          ),
-        );
-      }
-    }
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
-    _calendarEventController.removeWhere((x) => true);
-    onRefresh();
-    return RefreshIndicator(
-      onRefresh: onRefresh,
-      child: Scaffold(
-        body: SafeArea(
-          child: DayView(
-            controller: _calendarEventController,
-            backgroundColor: Theme.of(context).colorScheme.surface,
-            heightPerMinute: 2,
-            headerStyle: HeaderStyle(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surfaceContainer,
-              ),
-              leftIconConfig: IconDataConfig(
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-              rightIconConfig: IconDataConfig(
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-            ),
-            scrollPhysics: BouncingScrollPhysics(),
-            onPageChange: (DateTime? time, int? i) {
-              if (time != null) {
-                if (getWeekOfMonth(time) != getWeekOfMonth(currentDate.value)) {
-                  currentDate.value = time;
-                  onRefresh();
-                } else {
-                  currentDate.value = time;
-                }
-              }
-            },
-          ),
+    return DayView(
+      controller: _historyController.calendarEventController,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      heightPerMinute: 2,
+      headerStyle: HeaderStyle(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceContainer,
+        ),
+        leftIconConfig: IconDataConfig(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+        rightIconConfig: IconDataConfig(
+          color: Theme.of(context).colorScheme.onSurface,
         ),
       ),
+      maxDay: DateTime.now(),
+      scrollPhysics: BouncingScrollPhysics(),
+      onPageChange: (DateTime? time, int? i) {
+        if (time != null) {
+          if (getWeekOfMonth(time) !=
+              getWeekOfMonth(_historyController.currentDate.value)) {
+            _historyController.currentDate.value = time;
+            _historyController.onRefresh();
+          } else {
+            _historyController.currentDate.value = time;
+          }
+        }
+      },
     );
   }
 }
--- client/lib/pages/home/home_screen.dart
+++ client/lib/pages/home/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:quick_attendance/components/primary_button.dart';
 import 'package:quick_attendance/controllers/auth_controller.dart';
+import 'package:quick_attendance/controllers/history_controller.dart';
 import 'package:quick_attendance/controllers/profile_controller.dart';
+import 'package:quick_attendance/pages/home/history_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,34 +15,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final AuthController authController = Get.find();
   final ProfileController profileController = Get.find();
+  final HistoryController _historyController = Get.find();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Obx(
-              () =>
-              // Greeting to the user
-              Text(
-                "Hello, ${profileController.firstName}",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      child: RefreshIndicator(
+        onRefresh: _historyController.onRefresh,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Obx(
+                () => Text(
+                  "Hello, ${profileController.firstName}",
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              "Welcome back! Here's what's happening right now.",
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            SizedBox(height: 24),
-            PrimaryButton(text: "Logout", onPressed: authController.logout),
-            PrimaryButton(
-              text: "Camera",
-              onPressed: () => Get.toNamed("/capture-attendance"),
-            ),
-          ],
+              SizedBox(height: 8),
+              Text(
+                "Welcome back! Here's what's happening right now.",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 24),
+
+              // Give HistoryScreen a defined height
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: HistoryScreen(),
+              ),
+            ],
+          ),
         ),
       ),
     );
